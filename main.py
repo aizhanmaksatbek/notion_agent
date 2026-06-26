5a54a9598825c4c593433d8d07a34702db3ea064
Aizhan <aizhan1maks@gmail.com>
2026-06-26 11:41:01 +0800

    Added a tool connect_db.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from langchain.agents import create_agent
-from prompts import SYSTEM_PROMPT
+from prompts import SYSTEM_PROMPT, content
+from langchain.tools import tool
 
 
-def search_articles(topic: str) -> str:
+@tool
+def search_articles(articles: str) -> str:
     """Searches articles for a given topic."""
-    return f"Articles list in {topic}!"
+    return f"Articles list in {articles}!"
+
+
+@tool
+def connect_db():
+    """This function loads the document for topic search"""
+    return content
 
 
 agent = create_agent(
     model="openai:gpt-5.5",
-    tools=[search_articles],
+    tools=[search_articles, connect_db],
     system_prompt=SYSTEM_PROMPT,
 )
 
 result = agent.invoke(
-    {"messages": [{"role": "user", "content": "What are the articles in personal expenses?"}]}
+    {"messages": [{"role": "user", "content": "Find articles related to personal expenses?"}]}
 )
+
 print(result["messages"][-1].content_blocks)
