ca99f53e496adc60c79c5142f8b3a07216d0dcab
Aizhan <aizhan1maks@gmail.com>
2026-06-26 12:25:58 +0800

    Added the pydantic schemas for instructions, and ouput formats.

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel, Field
+from typing import Tuple, List
 
 
 class InputPrompt(BaseModel):
     text: str = Field(..., max_length=50)
+
+
+class PromptInstruction(BaseModel):
+    instruction: str
+    status: Tuple
+
+
+class AgentOutput(BaseModel):
+    result: List[PromptInstruction]
